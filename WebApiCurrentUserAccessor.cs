f4a8e66be3726c34bd4d904c202a558474c4898d
Benjamin <benjaminjcharlton@gmail.com>
2021-03-31 17:33:54 +0400

    A class to access the current user from command handlers

---
+++ WebApiCurrentUserAccessor.cs
@@ -0,0 +1,26 @@
+﻿using Energetic.Security;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Security.Claims;
+
+namespace Energetic.WebApis
+{
+    // TODO: Move this to an appropriate assembly and namespace within the Energetic libaries
+    public class WebApiCurrentUserAccessor : ICurrentUserAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public WebApiCurrentUserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        public ClaimsPrincipal CurrentUser
+        {
+            get
+            {
+                return _httpContextAccessor.HttpContext.User;
+            }
+        }
+    }
+}
